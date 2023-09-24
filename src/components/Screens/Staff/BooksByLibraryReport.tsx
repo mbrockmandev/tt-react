d3ca2beeb6ff1c8767b8e4eec262fa81811d401d
Mike Brockman <mbrockmandev@gmail.com>
2023-09-24 19:52:11 -0400

    update grid settings for books report

--- src/components/Screens/Staff/BooksByLibraryReport.tsx
+++ src/components/Screens/Staff/BooksByLibraryReport.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {useRecoilState} from "recoil";
+import { useRecoilState } from "recoil";
 
-import {booksByLibraryAtom} from "../../../recoil/atoms/booksByLibraryAtom";
+import { booksByLibraryAtom } from "../../../recoil/atoms/booksByLibraryAtom";
 
 import BookCard from "../../Book/BookCard";
 
@@ -14,19 +14,20 @@ const BooksByLibraryReport = () => {
     useRecoilState(booksByLibraryAtom);
 
   const displayedBooks = booksByLibrary.books.slice(
-    (booksByLibrary.page - 1) * ITEMS_PER_PAGE, booksByLibrary.page * ITEMS_PER_PAGE);
-  
+    (booksByLibrary.page - 1) * ITEMS_PER_PAGE,
+    booksByLibrary.page * ITEMS_PER_PAGE,
+  );
+
   return (
     <>
       <div className="flex items-center justify-center w-screen gap-3 pt-4">
         <div className="main-section flex-1 p-4 space-y-4">
           <h1 className="font-black text-3xl">
-            {booksByLibrary.totalPages * 10} different titles at
-            Library: {booksByLibrary.libraryId} with a total
-            of {booksByLibrary.totalBookCount} total books!
+            {booksByLibrary.totalPages * 10} different titles at Library:{" "}
+            {booksByLibrary.libraryId} with a total of{" "}
+            {booksByLibrary.totalBookCount} total books!
           </h1>
-          <div
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-4">
             {displayedBooks.map((b) => (
               <BookCard
                 key={`${b.book.id}`}
@@ -40,10 +41,12 @@ const BooksByLibraryReport = () => {
       <PaginationNumbers
         page={booksByLibrary.page}
         totalPages={booksByLibrary.totalPages}
-        onPageChange={(newPage) => setBooksByLibrary({
-          ...booksByLibrary,
-          page: newPage,
-        })}
+        onPageChange={(newPage) =>
+          setBooksByLibrary({
+            ...booksByLibrary,
+            page: newPage,
+          })
+        }
       />
     </>
   );
